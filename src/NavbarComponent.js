e6ea465fe2fc3c1b046d4de91b185a5c95910371
shananigans <shana.ahemode@gmail.com>
2024-10-14 22:51:13 -0400

    navbar styling updated ~

--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -1,14 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavbarComponent = () => {
+const NavbarComponent = ( {setTheme, theme }) => {
+
+    const handleThemeChange = (e) => {
+        const selectedTheme = e.target.value;
+        setTheme(selectedTheme);
+    };
+
+    const baseColor = theme === 'dark' ? 'bg-darkBlue text-customOrange' : 'bg-customOrange text-darkBlue';
+    const borderColor = theme === 'dark' ? 'border-customOrange' : 'border-darkBlue';
+
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Timer</Link></li>
-                <li><Link to="/stats">Stats</Link></li>
-            </ul>
-        </nav>
+        <nav className={`font-mono shadow flex justify-between items-center py-4 px-8 border rounded-xl mx-[2vw] mt-4 mb-4 ${borderColor}`}>
+            <div className={`flex space-x-4 ${baseColor}`}>
+                <Link 
+                    to="/"
+                    className="font-semibold text-lg cursor-pointer"
+                >
+                    tracker
+                </Link>
+                <Link
+                    to="/stats"
+                    className="font-semibold text-lg cursor-pointer"
+                >
+                    stats
+                </Link>
+            </div>
+        <select
+            value={theme}
+            onChange={handleThemeChange}
+            className={`appearance-none bg-transparent font-semibold cursor-pointer ${baseColor}`}
+        >
+            <option value="light">light mode</option>
+            <option value="dark">dark mode</option>
+        </select>     
+    </nav>
     );
 }
 
